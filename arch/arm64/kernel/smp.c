7f4db234d59d94d41c1c78dc0940f1050c5bc84a
Sultan Alsawaf <sultan@kerneltoast.com>
2020-04-23 14:38:39 -0700

    arm64: Allow IPI_WAKEUP to be used outside of the ACPI parking protocol
    An empty IPI is useful for cpuidle to wake sleeping CPUs without causing
    them to do unnecessary work upon receipt of the IPI. IPI_WAKEUP fills
    this use-case nicely, so let it be used outside of the ACPI parking
    protocol.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- arch/arm64/kernel/smp.c
+++ arch/arm64/kernel/smp.c
@@ -736,12 +736,10 @@ void arch_send_call_function_single_ipi(int cpu)
 	smp_cross_call(cpumask_of(cpu), IPI_CALL_FUNC_SINGLE);
 }
 
-#ifdef CONFIG_ARM64_ACPI_PARKING_PROTOCOL
 void arch_send_wakeup_ipi_mask(const struct cpumask *mask)
 {
 	smp_cross_call(mask, IPI_WAKEUP);
 }
-#endif
 
 #ifdef CONFIG_IRQ_WORK
 void arch_irq_work_raise(void)
@@ -831,13 +829,8 @@ void handle_IPI(int ipinr, struct pt_regs *regs)
 		break;
 #endif
 
-#ifdef CONFIG_ARM64_ACPI_PARKING_PROTOCOL
 	case IPI_WAKEUP:
-		WARN_ONCE(!acpi_parking_protocol_valid(cpu),
-			  "CPU%u: Wake-up IPI outside the ACPI parking protocol\n",
-			  cpu);
 		break;
-#endif
 
 	case IPI_SGI_15_IRQ:
 		pr_debug("%s: Interrupt masking bit of core [%dth] is cleared by "
